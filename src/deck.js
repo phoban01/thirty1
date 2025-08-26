de8317ddea587843bc7b0761ce369490162c6337
Piaras Hoban <4415593+phoban01@users.noreply.github.com>
2025-08-26 12:20:05 +0100

    feat: add card deck utility

---
+++ src/deck.js
@@ -0,0 +1,35 @@
+const SUITS = ['hearts', 'diamonds', 'clubs', 'spades'];
+const RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+function createDeck() {
+  const deck = [];
+  for (const suit of SUITS) {
+    for (const rank of RANKS) {
+      deck.push({ rank, suit });
+    }
+  }
+  return deck;
+}
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return deck;
+}
+
+function draw(deck) {
+  return deck.pop();
+}
+
+function discard(card, pile) {
+  pile.push(card);
+}
+
+module.exports = {
+  createDeck,
+  shuffle,
+  draw,
+  discard,
+};
